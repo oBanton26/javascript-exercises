9c22ec9a02b0c2e6409afac5e5ecafc1adad4990
Ulysse <ulysse.aubanton@gmail.com>
2025-03-26 20:11:37 +0100

    Add correct function
    I had a small hard time to figure out why my function with the slice
    method won't work... until I understood that it slices everything until
    the end if you have only 1 argument. So I simply used the at() method
    (which is perfect because it newly accept negative numbers like I wanted
    to).

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedWord = "";
+    let transferString = "";
+    for (let i=1; i <= word.length; i++){
+        transferString = word.at(-i);
+        reversedWord += transferString;
+    }
+    return reversedWord;
 };
 
 // Do not edit below this line
