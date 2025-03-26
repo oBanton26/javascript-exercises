f9bd9c16081568621967be1aa1278f305fd027c5
Ulysse <ulysse.aubanton@gmail.com>
2025-03-26 19:38:26 +0100

    Add function that passes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, numb) {
+    let finalString = "";
+    if (numb<0){return "ERROR"};
+    for (let i=1; i <= numb; i++) {
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
