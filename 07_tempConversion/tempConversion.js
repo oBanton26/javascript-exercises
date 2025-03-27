537f92cf9ac3a5ac80eb790750df96c4ecfec987
Ulysse <ulysse.aubanton@gmail.com>
2025-03-27 15:35:48 +0100

    Add the two functions for exercice seven
    Easy ! The only trouble was finding a way to keep only a number of
    decimals. The Math.round() is perfect, combined with (*10n)/10n so you
    choose the n number of decimals (here 1)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+  let convertedTemp = (degreeF-32)*(5/9);
+  let roundedConvertedTemp = Math.round(convertedTemp*10)/10;
+  return roundedConvertedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+  let convertedTemp = degreeC*(9/5)+32;
+  let roundedConvertedTemp = Math.round(convertedTemp*10)/10;
+  return roundedConvertedTemp;
 };
 
 // Do not edit below this line
