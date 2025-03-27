18628289da44f5d72f2451cea94c3bc74714e0cf
Ulysse <ulysse.aubanton@gmail.com>
2025-03-27 13:07:50 +0100

    Add function for  exercice 05
    Easy exercice, the diffculty was managing the exceptions in the input
    arguments. I used the Number.isInteger() method which checked if it was
    an integer, so excluding decimal but also all other data types.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+    if (intOne<0 || intTwo<0 || !Number.isInteger(intOne) || !Number.isInteger(intTwo)){
+        return "ERROR";
+    }
+
+    let bigInt;
+    let smallInt;
+    if (intOne > intTwo){
+        bigInt = intOne;
+        smallInt = intTwo;
+    } else {
+        bigInt = intTwo;
+        smallInt = intOne;
+    }
+
+    let sum = 0;
+    for (let i=smallInt; i <= bigInt; i++){
+        sum += i;
+    }
+
+    return sum;
 
 };
 
