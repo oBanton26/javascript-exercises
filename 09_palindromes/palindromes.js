8e93b5d3408f20adc3455d841feb07dd6c1759d9
Ulysse <ulysse.aubanton@gmail.com>
2025-04-11 12:48:36 +0200

    Complete first three tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    const punctuation = /[\.,?!]/g;
+    let myString = input.replace(punctuation, "");
+    myString = myString.toLowerCase();
+    const stringToCompare = myString.split("").reverse().join("");
+    if ( stringToCompare === myString){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
