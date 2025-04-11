9ee1ac64e2bb5caddb43b312ce9ee7c5818b8b27
Ulysse <ulysse.aubanton@gmail.com>
2025-04-11 11:21:12 +0200

    Finish exercice on calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,16 +10,23 @@ const sum = function(arr) {
   return arr.reduce((acc, item) => acc + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, item) => acc*item, 1);
 };
 
-const power = function() {
-	
+const power = function(numb, power) {
+	return numb ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(numb) {
+	if (numb == 0){return 1}
+  else {
+    let result = 1;
+    for (let i=1; i <= numb; i++){
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
