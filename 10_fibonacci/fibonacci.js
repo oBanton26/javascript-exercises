9cc98fd0fdf346b8ed6647ce891cf1ec2d9f4f24
Ulysse <ulysse.aubanton@gmail.com>
2025-04-11 13:37:37 +0200

    Create the first draft of the function
    It passes the 6 first tests. I used an array to store every number of
    the finonacci suite. I initiated it with 0 and 1 so it can return
    directly the index of the array with the number input (without the gap).
    Then a for loop creates the suite the size of the number input.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(numb) {
+    let fiboArray = [0, 1];
+    for (let i=2; i <= numb; i++){
+        fiboArray.push(fiboArray[i-1]+fiboArray[i-2]);
+    }
 
+    return fiboArray[numb]
 };
 
 // Do not edit below this line
